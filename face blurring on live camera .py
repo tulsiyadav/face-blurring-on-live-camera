6a450aaba03ce99ded3bfeaf07f21e987162946f
tulsiyadav <115484342+tulsiyadav@users.noreply.github.com>
2024-03-07 11:05:46 +0530

    Add files via upload

---
+++ face blurring on live camera .py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # EDUNET FOUNDATION- Self-Paced Solution
+
+# ## Lab 2 : Face Blurring on Live Video Stream
+# ## Problem Statement
+
+# ### Connect a video capture device to your laptop or computer. 
+# ### You need to implement a face blurring system that will capture from live camera stream and displays output in a seperate window. Follow guidelines discussed in classrom exercise
+
+# ### Importing warning package to ignore the warnings¶
+
+# In[1]:
+
+
+import warnings
+warnings.filterwarnings("ignore")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+# ### Load required libraries 
+
+# In[2]:
+
+
+import cv2
+
+
+# ### Load the cascade
+
+# In[3]:
+
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+# ### To use a video file as input 
+
+# In[4]:
+
+
+cap = cv2.VideoCapture(0)
+
+
+# ### Continual loop with face detection task
+
+# In[5]:
+
+
+while True:
+
+    _,img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        ROI = img[y:y+h, x:x+w]
+        blur = cv2.GaussianBlur(ROI, (91,91),0) 
+        img[y:y+h, x:x+w] = blur
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    cv2.imshow('img', img)
+    k = cv2.waitKey(300) & 0xff
+    if k==27:
+        break
+
+
+# ### Release the VideoCapture object
+# 
+
+# In[6]:
+
+
+cv2.destroyAllWindows()
+cap.release()
+
+
+# In[ ]:
+
+
+
+
